aa734115a80b878c2a6b6426daa1b128fea9d29e
AstaYC PS <baihmohamedamine@gmail.com>
2024-10-28 17:01:48 +0100

    first step , new concept

---
+++ src/main/java/com/astayc/ebankiboot/EBankiBootApplication.java
@@ -0,0 +1,13 @@
+package com.astayc.ebankiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankiBootApplication.class, args);
+	}
+
+}

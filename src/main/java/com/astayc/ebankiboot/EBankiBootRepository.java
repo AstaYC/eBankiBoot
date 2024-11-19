6b93629ac304a43f359086e6da53284ddb29464b
AstaYC PS <baihmohamedamine@gmail.com>
2024-11-19 10:13:26 +0100

    just for enter into the beat

--- src/main/java/com/astayc/ebankiboot/EBankiBootRepository.java
+++
@@ -1,7 +0,0 @@
-package com.astayc.ebankiboot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EBankiBootRepository extends  JpaRepository<EBankiBoot , Long>{
-
-}
